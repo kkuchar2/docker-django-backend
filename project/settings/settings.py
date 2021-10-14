7d47a816db8c5ed5d46a283c5131e0df054c5181
Krzysztof Kucharski <krzysiekkucharski7@gmail.com>
2021-10-14 13:19:39 +0000

    Add Sentry IO support

--- project/settings/settings.py
+++ project/settings/settings.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
-
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from util import envv
 import os
 
@@ -202,6 +203,30 @@ AVAILABLE_MODELS = [
     'apps.covid.models.CovidStats',
     'apps.covid.models.CovidCalcs',
 ]
+
+"""
+Sentry logging
+"""
+sentry_sdk.init(
+    dsn=envv('SENTRY_DSN_URL'),
+    integrations=[DjangoIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
+
 
 if not PRODUCTION_ENV:
     from .development import *
